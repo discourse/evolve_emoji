1b4a4e536ee157f82e883cc70b66153a8dc90577
Robin Ward <robin.ward@gmail.com>
2014-11-03 15:48:56 -0500

    Evolve Emoji initial commit

---
+++ assets/javascripts/evolve-emoji.js
@@ -0,0 +1,7 @@
+(function() {
+  Discourse.Dialect.registerEmoji('assault', 'https://talk.turtlerockstudios.com/uploads/default/7462/349fad39a4d83fa6.png');
+  Discourse.Dialect.registerEmoji('medic',   'https://talk.turtlerockstudios.com/uploads/default/7463/6d49d7ef690f7513.png');
+  Discourse.Dialect.registerEmoji('monster', 'https://talk.turtlerockstudios.com/uploads/default/7464/ac4b26e7ee4dcb02.png');
+  Discourse.Dialect.registerEmoji('support', 'https://talk.turtlerockstudios.com/uploads/default/7465/97b42f33a2b24d82.png');
+  Discourse.Dialect.registerEmoji('trapper', 'https://talk.turtlerockstudios.com/uploads/default/7466/d3c41d1b8d90e312.png');
+})();
---
+++ plugin.rb
@@ -0,0 +1,6 @@
+# name: evolve_emoji
+# about: adds in evolve specific emoji
+# version: 0.1
+# authors: Robin Ward
+
+register_asset "javascripts/evolve-emoji.js", :server_side
